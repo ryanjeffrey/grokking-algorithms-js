af85f3bc42038a6f2339c612ce17742c781854ca
Ryan Smith <ryanjeffreysmith@gmail.com>
2022-12-20 16:04:57 -0600

    Write greet function to learn about the call stack

--- 03-recursion.js
+++ 03-recursion.js
@@ -5,3 +5,22 @@ function countdown(i) {
 }
 
 countdown(5);
+
+////////////////
+
+function greet2(name) {
+    console.log("how are you, " + name + "?");
+}
+
+function bye() {
+    console.log("ok bye!");
+}
+
+function greet(name) {
+    console.log("hello, " + name + "!");
+    greet2(name);
+    console.log("getting ready to say bye...");
+    bye();
+}
+
+greet("Ryan");
