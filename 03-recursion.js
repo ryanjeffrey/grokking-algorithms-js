6ab907523e3cbbf82adc268dfb01e32e9631af9b
Ryan Smith <ryanjeffreysmith@gmail.com>
2022-12-20 16:07:30 -0600

    Write recursive function to calculate factorials

--- 03-recursion.js
+++ 03-recursion.js
@@ -24,3 +24,12 @@ function greet(name) {
 }
 
 greet("Ryan");
+
+////////////////
+
+function factorial(x) {
+    if (x === 0) return 1;
+    return x * factorial(x - 1);
+}
+
+console.log(factorial(5));
