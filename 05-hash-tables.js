dd45a75327a4cafcd8d263d6fa904620f7b010a8
Ryan Smith <ryanjeffreysmith@gmail.com>
2022-12-26 16:02:23 -0600

    Create hash table to store names and voting status

--- 05-hash-tables.js
+++ 05-hash-tables.js
@@ -5,3 +5,20 @@ book['milk'] = 1.49;
 book['avocado'] = 1.49;
 
 console.log(book);
+
+////////////////
+
+const voted = {};
+
+function checkVoter(name) {
+    if (voted[name]) {
+        console.log('kick them out!');
+    } else {
+        voted[name] = true;
+        console.log('let them vote!');
+    }
+}
+
+checkVoter('Justin');
+checkVoter('Rashid');
+checkVoter('Rashid');
